8572c4f95a2e2d18487a7f808867f962aaab7d3b
Nikhil upadhyay <nikhilupadhyay1604@gmail.com>
2025-05-04 13:18:49 +0530

    add: forEach polyfill

---
+++ Polyfills/forEach.js
@@ -0,0 +1,15 @@
+// forEach polyfill
+
+Array.prototype.myForEach = (cb) => {
+  for (let index = 0; index < this.length; index++) {
+    cb(this[i]);
+  }
+};
+
+const cb = (index) => {
+  console.log(index * 10);
+};
+
+const arr = [10, 20, 30];
+
+arr.forEach(cb);
