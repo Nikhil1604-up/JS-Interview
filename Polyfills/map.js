3f0f1325a3b335f08774a552ecc7f90010530e22
Nikhil upadhyay <nikhilupadhyay1604@gmail.com>
2025-05-04 18:43:46 +0530

    add: map polyfill implementation

--- Polyfills/map.js
+++ Polyfills/map.js
@@ -1,1 +1,14 @@
 // map polyfill
+
+const arr = [5, 10, 15, 20, 25];
+
+Array.prototype.myMap = function (cb) {
+  let result = [];
+
+  for (let index = 0; index < this.length; index++) {
+    result.push(cb(this[index], index, this));
+  }
+  return result;
+};
+
+const resultArr = arr.myMap((el) => el * 5);
